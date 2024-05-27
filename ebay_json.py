91d4d64126c2db43b258866650fc1538b9980536
Guhan <128699100+Guhanarull@users.noreply.github.com>
2024-05-28 00:18:34 +0530

    Update ebay_json.py

--- ebay_json.py
+++ ebay_json.py
@@ -1,16 +1,21 @@
 import json
 import requests
 from bs4 import BeautifulSoup
-url = "https://www.ebay.com/itm/204275338902"
+
+url = "https://anchorexpress.com/products/furuno-infolink-bbwx4-satellite-weather-receiver"
+# url = "https://www.ebay.com/itm/204275338902"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-price_tag = soup.find_all("script", {"type": "application/ld+json"})
+# Extract all scripts of type "application/ld+json"
+data_types = soup.find_all("script", {"type": "application/ld+json"})
 
-for item in price_tag:
-    script_text = item.string
-    json_data = json.loads(script_text)
-    if json_data.get('@type') == 'Product' and 'offers' in json_data:
-            price = json_data['offers']['price']
-            print("Price:", price)
+for item in data_types:
+    data_types = json.loads(item.string)
+    if data_types.get("@type") == "Product":
+            hi = data_types['offers']
+            for offer in hi:
+                price = offer.get('price')
+                print("Price:", price)
+
